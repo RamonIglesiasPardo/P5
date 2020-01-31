fd2b89eea1022acd0244e3faf8e3f1dd176a752c
beto-uoc <agonzalezcasad@fp.uoc.edu>
2020-01-31 19:29:44 +0100

    Creado paquete ong.sql con la clase Prueba_Sql, que se conecta con una base de datos y muestra una consulta simple.

---
+++ src/ong/sql/Prueba_Sql.java
@@ -0,0 +1,39 @@
+package ong.sql;
+
+import java.sql.*;
+
+public class Prueba_Sql {
+
+    public static void main(String[] args) {
+
+        try {
+
+            //1.Creamos una conexion a la base de datos mediante un objeto Connection
+
+            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3307/entreculturas?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                    "root", "");
+
+            //2.Creamos un objeto Statement, para poder crear una sentencia después
+
+            Statement miStatement = miConexion.createStatement();
+
+            //3.Creamos un objeto ResultSet para ejecutar una sentencia SQL utilizando el método executeQuery()
+
+            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM Personal;");
+
+            //4.Utilizamos un ciclo while para mostrar los campos indicados mediante el método getString()
+
+            while (miResultSet.next()) {
+
+                System.out.println(miResultSet.getString("idPersonal") + " - " +
+                        miResultSet.getString("segundoApellido") + " " + miResultSet.getString("primerApellido") + ", " +
+                        miResultSet.getString("nombre") );
+            }
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión.");
+            e.printStackTrace();
+        }
+
+    }
+
+}
