20f4ee0d9d4d7d19432152cefd6352e3e6eb024b
beto-uoc <agonzalezcasad@fp.uoc.edu>
2020-02-01 19:03:26 +0100

    Actualizado ONG.sql

--- src/sql/ONG.sql
+++ src/sql/ONG.sql
@@ -3,6 +3,60 @@
    *  y sus tablas.
    */
 
+-- Creamos el esquema de la base de datos
+
+CREATE SCHEMA IF NOT EXISTS Entreculturas DEFAULT CHARACTER SET utf8mb4;
+USE Entreculturas;
+
+/** TABLA PERSONA
+ *  Para implementar la herencia he optado por crear un campo PK, IdPersona,
+ *  que utilizaremos como referencia para asociar las subclases. Cada subclase
+ *  tendrá además su id propio, más que nada para identificar cada instancia en
+ *  SQL, y tendremos el campo IdPersonal en la tabla Personal que será el verdadero
+ *  Id de empleado general que hemos generado en Java.
+ */
+
+CREATE TABLE IF NOT EXISTS Persona (
+
+	IdPersona INT(6) NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(16) NOT NULL,
+    PrimerApellido VARCHAR(16) NOT NULL,
+    SegundoApellido VARCHAR(16) NOT NULL,
+    -- Opto por que la deireccion sea de tipo VARCHAR, por dos motivos:
+    -- Primero, en teoría debemos pasar los datos desde el XML generado por
+    -- la aplicación Java, que es una cadena de texto en realidad.
+    -- Segundo, podríamos crear una tabla Direccion con los campos corresponientes
+    -- (Calle, piso, puerta, etc.), pero no le veo mucho sentido porque, para qué
+    -- almacenar las direcciones en una tabla aparte... Si lo que importa es que
+    -- pasemos los elementos desde Java, concretamente desde un fichero XML, creo
+    -- que el campo Direccion de tipo VARCHAR nos vale perfectamente.
+    Direccion VARCHAR(128) NOT NULL,
+    Telefono VARCHAR(12) NOT NULL,
+    Mail VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY(IdPersona)
+
+) ENGINE = InnoDB;
+
+/** TABLA PERSONAL
+ *
+ * Con la FK IdPersona podemos relacionar esta tabla Personal con su superclase Persona.
+ * Para realizar una consulta de una instancia Personal, deberíamos hacer un JOIN entre
+ * la tabla Personal y Persona, para mostrar los campos de la superclase (nombre, teléfono, etc.)
+ */
+
+CREATE TABLE IF NOT EXISTS Personal (
+
+	IdPersonal INT(4) NOT NULL AUTO_INCREMENT,
+    IdPersona INT(6) NOT NULL,
+
+    PRIMARY KEY(IdPersonal),
+    FOREIGN KEY(IdPersona) REFERENCES Persona(IdPersona)
+		ON UPDATE CASCADE ON DELETE CASCADE
+
+) ENGINE = InnoDb;
+
+
   /** TABLA PerVoluntario
    *
    * Lo mismo que las anteriores, pero aquí se incluirán más referencias
